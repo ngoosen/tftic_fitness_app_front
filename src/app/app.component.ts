641652cde606ff41627fb1775846439ebab2516f
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-17 14:31:01 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'tftic_fitness_app_front';
+}

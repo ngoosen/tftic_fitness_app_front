11e705dd1e1d617ae770fc00dd0c50b49edcc407
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-18 16:57:31 +0100

    fix: replace env file

--- src/app/tools/services/measure.service.ts
+++ src/app/tools/services/measure.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environments';
 import { Measure } from '../../models/measure.model';
-
-const { API_MAIN_PATH } = process.env;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeasureService {
+  baseUrl = environment.apiUrl;
 
   constructor(private _http: HttpClient) { }
 
   getMeasures(): Observable<Measure[]> {
-    return this._http.get<Measure[]>(`${API_MAIN_PATH}/measure`);
+    return this._http.get<Measure[]>(`${this.baseUrl}/measure`);
   }
 }

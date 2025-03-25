e4e08fe51420741e33d837bd4a4934dbb4f561de
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-25 14:50:24 +0100

    feat: add search function to exercises page

--- src/app/pages/exercises/exercises.component.ts
+++ src/app/pages/exercises/exercises.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cleanString } from '../../lib/helpers/cleanString';
 import { Exercise } from '../../models/exercise.model';
 import { ExerciseService } from '../../tools/services/exercise.service';
 
@@ -10,15 +11,44 @@ import { ExerciseService } from '../../tools/services/exercise.service';
 })
 export class ExercisesComponent {
   exercises: Exercise[] = [];
+  displayedExercises: Exercise[] = [];
 
   constructor (private _exerciseService: ExerciseService) {
     _exerciseService.getExercises().subscribe({
       next: (data) => {
         this.exercises = data;
+        this.displayedExercises = data;
       },
       error: (e) => {
         console.log(e);
       }
     });
+  }
+
+  search(event: any) {
+    const value = event.target.value.toLowerCase();
+
+    if (value === "") {
+      this.displayedExercises = this.exercises;
+    } else {
+      this.displayedExercises = this.exercises.filter(exercise => {
+        const exerciseName = cleanString(exercise.exercise_name);
+        const terms = exerciseName.split(" ");
+
+        let match = false;
+
+        terms.map(term => {
+          if (term.startsWith(value)) {
+            match = true;
+          }
+        });
+
+        if (match) {
+          return match;
+        } else {
+          return exerciseName.startsWith(value);
+        }
+      });
+    }
   }
 }

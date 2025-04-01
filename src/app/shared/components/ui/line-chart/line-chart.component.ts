4cdb957878667d261ddd6fe5037c680e3cc664c2
Noémie Goosen <noemie.goosen@gmail.com>
2025-04-01 15:00:03 +0200

    feat: process data to chartjs data structure

--- src/app/shared/components/ui/line-chart/line-chart.component.ts
+++ src/app/shared/components/ui/line-chart/line-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ChartData } from 'chart.js';
 import dayjs from 'dayjs';
-import { LineChart } from '../../../../models/line-chart.model';
 import { TrainingSessionService } from '../../../../tools/services/training-session.service';
 
 @Component({
@@ -12,7 +12,7 @@ import { TrainingSessionService } from '../../../../tools/services/training-sess
 export class LineChartComponent {
   @Input() exerciseId: string = "";
 
-  lineChartConfig!: LineChart;
+  lineChartData!: ChartData<"line", number[], string>;
 
   constructor (private _trainingSessionService: TrainingSessionService) { }
 
@@ -64,70 +64,10 @@ export class LineChartComponent {
             });
           });
 
-          firstStep = firstStep.sort((step1, step2) => {
-            if (step1.measure_name < step2.measure_name) {
-              return -1;
-            }
-
-            if (step1.measure_name > step2.measure_name) {
-              return 1;
-            }
-
-            return 0;
-          });
-
-          const chartData: {
-            name: string;
-            series: {
-              value: number;
-              name: string;
-            }[]
-          }[] = [];
-
-          let currentMeasure = "";
-          let currentSeries: {
-            value: number;
-            name: string;
-          }[] = [];
-
-          firstStep.forEach(element => {
-            if (currentMeasure !== element.measure_name) {
-              if (currentMeasure !== "") {
-                currentSeries = currentSeries.sort((sery1, sery2) => {
-                  const date1 = dayjs(sery1.name);
-                  const date2 = dayjs(sery2.name);
-
-                  if (date1.isBefore(date2)) {
-                    return -1;
-                  }
-
-                  if (date1.isAfter(date2)) {
-                    return 1;
-                  }
-
-                  return 0;
-                });
-
-                chartData.push({
-                  name: currentMeasure,
-                  series: currentSeries,
-                });
-              }
-
-              currentMeasure = element.measure_name;
-              currentSeries = [];
-            }
-
-            currentSeries.push({
-              value: element.measure_quantity_average,
-              name: element.date,
-            });
-          });
-
-          if (currentSeries.length > 0) {
-            currentSeries = currentSeries.sort((sery1, sery2) => {
-              const date1 = dayjs(sery1.name);
-              const date2 = dayjs(sery2.name);
+          firstStep = firstStep
+            .sort((step1, step2) => {
+              const date1 = dayjs(step1.date);
+              const date2 = dayjs(step2.date);
 
               if (date1.isBefore(date2)) {
                 return -1;
@@ -138,18 +78,46 @@ export class LineChartComponent {
               }
 
               return 0;
+            })
+            .sort((step1, step2) => {
+              if (step1.measure_name < step2.measure_name) {
+                return -1;
+              }
+
+              if (step1.measure_name > step2.measure_name) {
+                return 1;
+              }
+
+              return 0;
             });
 
-            chartData.push({
-              name: currentMeasure,
-              series: currentSeries,
-            })
-          }
+          const labels: string[] = [];
 
-          this.lineChartConfig = {
-            xAxisLabel: "",
-            yAxisLabel: "",
-            data: chartData,
+          const datasets: {
+            label: string;
+            data: number[];
+          }[] = [];
+
+          firstStep.forEach(element => {
+            if (!labels.includes(element.date)) {
+              labels.push(dayjs(element.date).format("DD/MM/YYYY"));
+            }
+
+            const datasetMatch = datasets.findIndex(set => set.label === element.measure_name);
+
+            if (datasetMatch >= 0) {
+              datasets[datasetMatch].data.push(element.measure_quantity_average);
+            } else {
+              datasets.push({
+                label: element.measure_name,
+                data: [element.measure_quantity_average],
+              });
+            }
+          });
+
+          this.lineChartData = {
+            labels,
+            datasets,
           };
         },
         error: (e) => {
